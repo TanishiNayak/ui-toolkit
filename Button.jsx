988e647b39e1407b63eadb98cbd4b54451aa34d5
Tanishi Nayak <100431225+TanishiNayak@users.noreply.github.com>
2024-06-18 00:08:03 +0530

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import './Button.css';
+
+
+function Button() {
+
+    return(
+        <>
+       
+        <div className = "button-text">Sign On</div>
+        
+        
+        <div className = "Sign-On1">
+            Sign On
+
+        </div>
+        <div className = "Sign-On2">
+            Sign On
+
+        </div>
+
+        <div className = "learn-more">
+            Learn More
+
+        </div>
+
+        <div className = "learn-more2">
+            Learn More
+
+        </div>
+
+        <div className = "button-text">
+            Continue
+        </div>
+
+        <div className = "learn-more">
+            Get Started 
+        </div>
+
+        <div className = "learn-more">
+            Cancel
+        </div>
+
+        <div className = "learn-more2">
+            Search
+        </div>
+
+        <div className = "learn-more2">
+            Join Now
+        </div>
+        
+        </>
+    );
+}
+
+export default Button;
